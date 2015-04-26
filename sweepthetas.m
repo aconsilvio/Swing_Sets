e8aa7f9185010debb4045a82ac2fc3ac958d2a9b
adeaver <deaver.andrew@gmail.com>
2015-04-25 21:53:41 -0400

    Updated Sweep Theta Graph

--- sweepthetas.m
+++ sweepthetas.m
@@ -5,6 +5,9 @@ function sweepthetas(accuracy)
        max_height(i) = max(swingthetasweep(omegas(i)));
     end
     
-    plot(omegas, max_height)
+    plot(omegas, max_height, 'b', 'LineWidth', 3)
+    xlabel('Frequency of Pumping')
+    ylabel('Maximum Height Achieved')
+    title('Frequency of Pumping as a function of Maximum Height Achieved')
 
 end
